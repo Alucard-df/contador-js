438780b654baa16a67a6d1ec48eac87831fc9655
Fabiano <fas27@bol.com.br>
2022-06-20 20:56:38 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function contador() {
+    // declaração de variável
+    var num = document.getElementById("num").value;
+
+    // algoritmo
+    // enquanto... faça
+    while (num >= 0) {
+        alert(num);
+        num--;
+    }
+}
